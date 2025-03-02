28ce11426021adeefe66a18d3d43a2c050fdc544
TitiOS <106855843+TitiOS@users.noreply.github.com>
2025-03-02 17:32:34 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Programa que imprime texto na tela
+ * @author Thiago de Oliveira Silva
+ */
+
+//O nome dado a classe deve ter o mesmo nome do arquivo.
+public class HelloWorld {
+    /* Metodo principal da classe */
+    public static void main(String[] args) {
+        System.out.println("\tOi Mundo!");//Na saida do sistema operacional estou impriminto "Oi Mundo!"
+        System.out.println("Java \n\"Standard Edition\"");
+    }// fim do metodo 
+}// fim da classe
